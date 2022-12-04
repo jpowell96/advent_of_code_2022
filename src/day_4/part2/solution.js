4c1a79f0e2ebfcc8ca96d473f4dea0e1bcfb377c
Jonathan Powell <powell.jonj@gmail.com>
2022-12-04 18:29:26 -0500

    Got the right answer, but still has a failing test

--- src/day_4/part2/solution.js
+++ src/day_4/part2/solution.js
@@ -5,7 +5,7 @@ var fs = require("fs");
 var AssignmentPair = /** @class */ (function () {
     function AssignmentPair(input) {
         // 1. Split on comma to get left / right
-        // ["2-4","6-8"]
+        // ["2-4"], ["6-8"]
         var _a = input.split(","), left = _a[0], right = _a[1];
         var leftAssignment = left.split("-");
         this.left = [Number.parseInt(leftAssignment[0]), Number.parseInt(leftAssignment[1])];
@@ -15,12 +15,23 @@ var AssignmentPair = /** @class */ (function () {
     return AssignmentPair;
 }());
 // Returns true if a overlaps b (left.0 <= right.0 && left.1 >= right.1)
+// [6,8], [2,4]
+// 6 <= 2 && 8 >= 2
+// 6 <= 2 &&  8 >= 4
+// [2, 4] [6,8]
 function overlaps(left, right) {
-    return left[0] <= right[0] || left[1] >= right[1];
+    if (left[0] > right[0] && left[1] > right[1]) {
+        return false;
+    }
+    // Case where left partially overlaps with right: [[2 , 5], [4,6]]
+    return (left[0] <= right[0] && left[1] >= right[0]) ||
+        // Case where right is a subset of left at the end point: [[2, 5] , [4,5]]
+        // 2 <= 4 && 5 >= 5
+        (left[0] <= right[0] && left[1] >= right[1]);
 }
 exports.overlaps = overlaps;
 try {
-    var data = fs.readFileSync('../sample_input.txt', 'utf8');
+    var data = fs.readFileSync('../input.txt', 'utf8');
     var assignmentPairs = data.split("\n");
     var numberOfOverlaps = assignmentPairs
         .map(function (assignmentPair) { return new AssignmentPair(assignmentPair); })
