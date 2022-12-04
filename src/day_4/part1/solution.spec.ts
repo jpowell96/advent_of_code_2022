f3cd9ec5cdda2ddb10c8c53786296ded186f28ca
Jonathan Powell <powell.jonj@gmail.com>
2022-12-04 16:14:15 -0500

    Some cleanup

--- src/day_4/part1/solution.spec.ts
+++ src/day_4/part1/solution.spec.ts
@@ -1,29 +1,28 @@
-import {countOverlappingPairs} from "./solution";
-
+import {overlaps} from "./solution";
 
 test("Array with no overlaps returns 0", () => {
     // Arrange: 
     const input = [[2,5], [5, 7]];
 
     // Act:
-    const result = countOverlappingPairs(input);
+    const result = overlaps(input[0], input[1]);
     
     // Assert:
-    expect(result).toBe(0);
+    expect(result).toBe(false);
 });
 
 test("Array with overlapping range returns 1", () => {
-    const input = [[2,4], [2-3]];
+    const input = [[2,4], [2,3]];
 
-    const result = countOverlappingPairs(input);
+    const result = overlaps(input[0], input[1]);
 
-    expect(result).toBe(1);
+    expect(result).toBe(true);
 });
 
 test("Array with overlapping ranges out of sort order returns 1", () => {
     const input = [[2,4], [1-30]];
 
-    const result = countOverlappingPairs(input);
+    const result = overlaps(input[0], input[1]);
 
-    expect(result).toBe(1);
+    expect(result).toBe(false);
 });
--- src/day_4/part1/solution.ts
+++ src/day_4/part1/solution.ts
@@ -1,10 +1,42 @@
 // Question to answer: In how many assignment pairs does one range fully contain the other?
+const fs = require("fs");
 
-export function countOverlappingPairs(assignments: number[][]) : number {
-    return -1;
+class AssignmentPair {
+    readonly left : number[];
+    readonly right: number[];
+
+    constructor(input: String) {
+        // 1. Split on comma to get left / right
+        // ["2-4","6-8"]
+
+        const [left, right] = input.split(",");
+        
+        const leftAssignment = left.split("-");
+        this.left = [Number.parseInt(leftAssignment[0]), Number.parseInt(leftAssignment[1])];
+        
+        const rightAssignment = right.split("-");
+    
+        this.right = [Number.parseInt(rightAssignment[0]), Number.parseInt(rightAssignment[1])];
+    }
+
 }
 
-// Returns true if a overlaps b, or if b overlaps a
-export function overlaps(assignmentA: number[], assignmentB: number[]) : boolean {
-    return false;
+try {
+   const data = fs.readFileSync('../input.txt', 'utf8');
+   const assignmentPairs : String[] = data.split("\n");
+   const numberOfOverlaps = assignmentPairs
+    .map(assignmentPair => new AssignmentPair(assignmentPair))
+    .map(pair => overlaps(pair.left, pair.right) || overlaps(pair.right, pair.left))
+    .filter(doesOverlap => doesOverlap)
+    .length;
+
+    console.log("We have %d overlaps", numberOfOverlaps);
+
+  } catch (err) {
+    console.error(err);
+  }
+
+// Returns true if a overlaps b (left.0 <= right.0 && left.1 >= right.1)
+export function overlaps(left: number[], right: number[]) : boolean {
+    return left[0] <= right[0] && left[1] >= right[1];
 }
